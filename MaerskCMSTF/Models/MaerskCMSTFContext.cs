bb0ee373481fe52bfa5602379d96167dc74e0e36
jingni15 <jingni_15@hotmail.com>
2017-07-10 11:14:49 +0800

    Add project files.

---
+++ MaerskCMSTF/Models/MaerskCMSTFContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MaerskCMSTF.Models
+{
+    public class MaerskCMSTFContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public MaerskCMSTFContext() : base("name=DefaultConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<MaerskCMSTF.Models.Ship> Ships { get; set; }
+
+        public System.Data.Entity.DbSet<MaerskCMSTF.Models.Container> Containers { get; set; }
+
+        public System.Data.Entity.DbSet<MaerskCMSTF.Models.YardLocation> YardLocations { get; set; }
+
+        public System.Data.Entity.DbSet<MaerskCMSTF.Models.ContainerShippingSchedule> ContainerShippingSchedules { get; set; }
+    }
+}
---
+++ MaerskCMSTF/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MaerskCMSTF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MaerskCMSTF")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("01e3c0d0-d851-42ff-b340-9d1ce7f79a89")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
